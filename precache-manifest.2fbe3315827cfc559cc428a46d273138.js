e2a93560fe75406e6e1ce9b88f8b5fe1565e3d85
jaredianmills <jaredianmills@gmail.com>
2018-11-05 18:27:35 -0500

    Updates

---
+++ precache-manifest.2fbe3315827cfc559cc428a46d273138.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/craftny-code-challenge/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8f92eae580daad949e7f",
+    "url": "/craftny-code-challenge/static/css/main.ba3e0552.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/craftny-code-challenge/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "ba8ced4e4c39f278f9d1",
+    "url": "/craftny-code-challenge/static/js/1.ba8ced4e.chunk.js"
+  },
+  {
+    "revision": "bfdccd232bcd357583a5",
+    "url": "/craftny-code-challenge/static/js/runtime~main.bfdccd23.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/craftny-code-challenge/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/craftny-code-challenge/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/craftny-code-challenge/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/craftny-code-challenge/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/craftny-code-challenge/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/craftny-code-challenge/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8f92eae580daad949e7f",
+    "url": "/craftny-code-challenge/static/js/main.8f92eae5.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/craftny-code-challenge/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/craftny-code-challenge/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/craftny-code-challenge/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/craftny-code-challenge/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/craftny-code-challenge/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/craftny-code-challenge/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/craftny-code-challenge/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/craftny-code-challenge/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ba8ced4e4c39f278f9d1",
+    "url": "/craftny-code-challenge/static/css/1.0f9dc5f5.chunk.css"
+  },
+  {
+    "revision": "7c20b397f4b70480d2a8227284c2fb5e",
+    "url": "/craftny-code-challenge/index.html"
+  }
+];
